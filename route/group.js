a75a5ac2f9e372353ed7b11eac24bba002dda5e3
perluto <nhhoanguet@gmail.com>
2021-05-17 16:02:46 +0700

    add all

--- route/group.js
+++ route/group.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const firebase = require("firebase-admin");
 const auth = require("../middleware/auth");
+const defaultAvatarURI =
+  "https://firebasestorage.googleapis.com/v0/b/chat-app-e54db.appspot.com/o/avatar%2Fdefault.jpg?alt=media&token=4760ffca-72fe-481c-97a9-58448d23fd6e";
 
 router.post("/", [auth], async (req, res) => {
   const { participants, name } = req.body;
@@ -13,6 +15,7 @@ router.post("/", [auth], async (req, res) => {
   const result = await groupsRef.add({
     participants,
     name,
+    avatar: defaultAvatarURI,
   });
 
   res.status(200).send({ idRoomChat: result.id, groupName: name });
@@ -29,8 +32,10 @@ router.get("/", [auth], async (req, res) => {
 
   if (snapshot.empty) return res.status(200).send([]);
   const groups = [];
+
   snapshot.forEach(async (doc) => {
-    groups.push({ id: doc.id, name: doc.data().name });
+    let tmp = doc.data();
+    groups.push({ id: doc.id, name: tmp.name, avatar: tmp.avatar });
   });
 
   res.status(200).send(groups);
